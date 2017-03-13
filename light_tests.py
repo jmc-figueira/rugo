e1d566c0ba644de198f8df92f113328382b8c96a
jcalixtofiguei <joao@wlan-145-94-253-053.wlan.tudelft.nl>
2017-03-13 13:59:59 +0000

    Removed temporary test file

--- light_tests.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def compute_light(base_color, light_color, light_intensity, absorb_rate, dx, dy):
-    absorbed = tuple(i * (1.0 - absorb_rate * math.sqrt(dx ** 2 + dy ** 2)) for i in light_color)
-    return tuple(max(min((b + l * light_intensity) / 2.0, 255), 0) for (b, l) in zip(base_color, absorbed))
